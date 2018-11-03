1cc344c6bb7cb77cafcd709bbc91c2d40590644d
Lucas Iacson <lucas.iacson@hotmail.com>
2018-11-03 19:13:52 -0300

    Conexión DB

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -40,6 +40,7 @@
 $autoload['packages'] = array();
 
 
+
 /*
 | -------------------------------------------------------------------
 |  Auto-load Libraries
@@ -52,7 +53,8 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array();
+// $autoload['libraries'] = array();
+$autoload['libraries'] = array('database','session');
 
 
 /*
